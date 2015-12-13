443c88c7b49a90fc62bc602d0e4c9fcf03db3858
Vinh <kurei@axcoto.com>
2015-12-13 14:35:46 -0800

    Started to do real work

--- Baby Kicking/PlayerCell.swift
+++ Baby Kicking/PlayerCell.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class PlayerCell: UITableViewCell {
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
 
+    
+    var player: Player! {
+        didSet {
+            gameLabel.text = player.game
+            nameLabel.text = player.game
+            ratingImageView.image = imageForRating(player.rating)
+        }
+    }
+    
+    func imageForRating(rating:Int) -> UIImage? {
+        let imageName = "\(rating)Stars"
+        return UIImage(named: imageName)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Baby Kicking/PlayersViewController.swift
+++ Baby Kicking/PlayersViewController.swift
@@ -40,27 +40,16 @@ class PlayersViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
+            as! PlayerCell
 
         // Configure the cell...
         let player = players[indexPath.row] as Player
-        
-        if let nameLabel = cell.viewWithTag(100) as? UILabel {
-            nameLabel.text = player.name
-        }
-        if let gameLabel = cell.viewWithTag(101) as? UILabel {
-            gameLabel.text = player.game
-        }
-        if let ratingImageView = cell.viewWithTag(102) as? UIImageView {
-            ratingImageView.image = self.imageForRating(player.rating)
-        }
+        cell.player = player
         
         return cell
     }
 
-    func imageForRating(rating:Int) -> UIImage? {
-        let imageName = "\(rating)Stars"
-        return UIImage(named: imageName)
-    }
+
     
     /*
     // Override to support conditional editing of the table view.
