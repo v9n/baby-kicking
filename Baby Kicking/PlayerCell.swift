40a37bb04a12678d450267c3a923343689b4d533
Vinh <kurei@axcoto.com>
2015-12-13 14:28:10 -0800

    Add carthage

---
+++ Baby Kicking/PlayerCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerCell.swift
+//  Baby Kicking
+//
+//  Created by Vinh Nguyen on 12/13/15.
+//  Copyright © 2015 Axcoto. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
